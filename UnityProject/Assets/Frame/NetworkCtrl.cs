6e8947b86f9a1794f2fdc2232854d39099bf0d92
JiangDi <761079730@qq.com>
2019-06-23 23:22:07 +0800

    添加network模块

---
+++ UnityProject/Assets/Frame/NetworkCtrl.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Oasis
+{
+    [DisallowMultipleComponent]
+    public class NetworkCtrl : MonoBehaviour
+    {
+       private NetworkCtrl()
+        {
+            //防止它人滥用实例化，导致报错
+        }
+    }
+}
